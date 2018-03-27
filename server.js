2d71e4b9fe031792cf191a78adea4d33635580fb
sarath <sarath.sct@gmail.com>
2018-03-27 18:47:04 +0530

    middleware

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
+const fs = require('fs');
+
 const express = require('express');
 const hbs = require('hbs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
-app.set('view engine','hbs');
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'))
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err){
+            console.log('Unable to write to server.log');
+        }
+    })
+    next();
+})
+
+app.use((req, res, next) => {
+    res.render('maintanence.hbs');
+})
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear()
@@ -22,15 +40,15 @@ app.get('/', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-    res.render('about.hbs',{
-        pageTitle : 'About'
+    res.render('about.hbs', {
+        pageTitle: 'About'
     })
 })
 
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Could not connect',
-        errorCode : 15632
+        errorCode: 15632
     })
 })
 
