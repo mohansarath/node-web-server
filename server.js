1f2d338d0c46736c0c4641e441b73635d5a07b92
sarath <sarath.sct@gmail.com>
2018-03-27 17:51:21 +0530

    handlebars

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine','hbs');
 app.use(express.static(__dirname + '/public'))
 
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
 app.get('/', (req, res) => {
-    res.send('<h1>Hello express !</h1>');
+    res.render('home.hbs', {
+        pageTitle: 'Home',
+        userName: 'Sarath'
+    })
 })
 
 app.get('/about', (req, res) => {
-    res.send('about page')
+    res.render('about.hbs',{
+        pageTitle : 'About'
+    })
 })
 
 app.get('/bad', (req, res) => {
